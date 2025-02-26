620c9f9d3d8e3fd9f93d887ec632ae0d5d2c4818
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2025-02-26 20:34:08 +0530

    create app.py

--- app.py
+++ app.py
@@ -15,9 +15,30 @@ st.markdown("2. Search by **course**. (e.g. 'Main Course', 'Dessert', 'Breakfast
 st.markdown("3. Search by **food dish**. (e.g. 'Biryani', 'Pizza', 'Pasta' etc.)")
 
 title = st.text_input("Type below:", placeholder="Hungry? Get yourself something to eat!", autocomplete="off").lower()
+submit_btn = st.button("Search")
 
-if title:
+if submit_btn:
     st.markdown("### Recommendations:")
-    st.write(recommend_dishes(title))
 
-# print(recommend_dishes("indian"))
+    # Assuming 'recommendations' is a DataFrame with 'name' and 'imgurl' columns
+    recommendations = recommend_dishes(title)
+    cols = st.columns(3, gap='medium', border=True, vertical_alignment='bottom')  # Creates 3 columns
+    
+    try:
+        with st.spinner("Searching...", show_time=True):
+            for i, (index, row) in enumerate(recommendations.iterrows()):
+                with cols[i % 3]:  # Distribute images across columns
+                    st.markdown(
+                        f"""
+                        <div style="display: flex; flex-direction: column; align-items: center; text-align: center;">
+                            <img src="{row['imgurl']}" style="width: 100%; height: 180px; object-fit: cover; border-radius: 10px;">
+                            <div style="height: 80px; display: flex; align-items: center; justify-content: center; margin-top: 12px; margin-bottom: 12px;">
+                                <p style="font-weight: bold; font-size: 1.1rem; margin: 8px; padding: 4px; text-align: center;">{row['name']}</p>
+                            </div>
+                        </div>
+                        """,
+                        unsafe_allow_html=True,
+                    )
+        st.success("Search succesfull!", icon="✔️")
+    except:
+        st.error("Unable to retrieve information.", icon="❌")
